59a4d3fc118d71c6444dd4e5eba2f87cd7cff4ac
Blake Huber <blakeca00@gmail.com>
2019-12-16 04:51:09 -0600

    init module add

---
+++ versionpro/__init__.py
@@ -0,0 +1,7 @@
+from versionpro.colors import Colors
+from versionpro._version import __version__ as version
+
+
+__author__ = 'Blake Huber'
+__version__ = version
+__email__ = "blakeca00@gmail.com"
